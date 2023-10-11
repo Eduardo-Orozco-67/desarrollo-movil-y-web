12a5a8e34e16a5fba818e396df90f36712d8a272
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2023-10-11 00:10:56 -0600

    examenn

--- app/index.php
+++ app/index.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Acceso de Usuarios</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Se agregó la metaetiqueta viewport para hacer la página responsive -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/cerulean/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles2.css">
 </head>
 <body>
-    <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="container py-5">
+        <div class="row justify-content-center align-items-center">
             <div class="col-12 col-md-8 col-lg-5">
                 <div class="card bg-dark text-white" style="border-radius: 1rem;">
                     <div class="card-body p-5 text-center">
@@ -25,7 +26,8 @@
                                     <input type="password" id="loginPassword" class="form-control form-control-lg" name="loginPassword" autocomplete="off" />
                                     <label class="form-label" for="loginPassword">Contraseña</label>
                                 </div>
-                                <button type="button" id="login" class="btn btn-outline-light btn-lg px-5">Ingresar</button>
+                                <div id="loginMessage" class="text-danger mb-3"></div>
+                                <button type="button" id="login" class="btn btn-outline-light btn-lg px-5 mt-5">Ingresar</button>
                             </form>
                         </div>
                     </div>
